bedbf3bbe7d029960d6aa2f48682340901d39d55
Omer Leibu <omer.leibu@gmail.com>
2021-12-25 11:53:20 +0200

    change server files

---
+++ Server Side/WebApplication1/Controllers/RecipeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RecipeController : ApiController
+    {
+
+        public List<Recipe> Get()
+        {
+            Recipe a = new Recipe();
+            return a.getRecipes();
+            
+        }
+    }
+}
